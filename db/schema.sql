7953eb3c5451d7f1642c354cdc8186680b60d17c
smedigo <smedigo@gmail.com>
2021-01-06 15:29:22 -0800

    worked on employee table insert

--- db/schema.sql
+++ db/schema.sql
@@ -18,14 +18,22 @@ CREATE TABLE role (
     ON DELETE CASCADE 
 );
 CREATE TABLE employee (
+
+    
     id INT unsigned AUTO_INCREMENT,
     first_name VARCHAR(300) NOT NULL,
     last_name VARCHAR(300) NOT NULL,
-    role_id INT unsigned NOT NULL,
+    role_id INT,
     manager_id INT unsigned,
     PRIMARY KEY (id),
-    CONSTRAINT fk_role FOREIGN KEY  (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE 
+    FOREIGN KEY  (role_id)
+    --CONSTRAINT (fk_role_id)
+    REFERENCES role(role_id) 
+    ON DELETE SET NULL;
+    --FOREIGN KEY (manager_id)
+    --CONSTRAINT (fk_manager_id),  
+    --REFERENCES employee(id)
+    -- ON DELETE SET NULL
 );
 
 
