cb383afad2140f71f3fb1d023876caf22a93ae19
smedigo <smedigo@gmail.com>
2021-01-05 17:10:21 -0800

    employeeDB

--- db/schema.sql
+++ db/schema.sql
@@ -5,34 +5,30 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT unsigned AUTO_INCREMENT,
+    name VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY (id),
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(30),
-    department_id INT,
+    id INT unsigned AUTO_INCREMENT,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL unsigned NOT NULL,
+    department_id INT unsigned NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id),
-    REFERENCES department(id),
-    ON DELETE SET NULL,
-
+    index dep_ind (department_id);
+    constraint fk_department foreign key (department_id) references department(id) on delete CASCADE 
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT unsigned AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
+    role_id INT unsigned NOT NULL,
+    manager_id INT unsigned,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id),
-    REFERECENES role(id),
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id),
-    REFERENCES employee(id),
-    ON DELETE SET NULL,
+    INDEX role_ind (role_id);
+    constraint fk_role foreign key (role_id) references role(id) on delete CASCADE 
+    index manager_ind (manager_id);
+    constraint fk_manager foreign key (manager_id) references employee(id) on delete CASCADE 
 );
