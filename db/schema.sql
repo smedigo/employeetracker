71e3980c08d40f36db4615cb1721b1bb8f3a41ec
smedigo <smedigo@gmail.com>
2021-01-06 19:50:52 -0800

    worked on role and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,17 @@ DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 USE employeeDB;
 CREATE TABLE department (
-    id INT unsigned AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     name VARCHAR(300) UNIQUE NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE role (
-    role_id INT unsigned AUTO_INCREMENT,
+    role_id INT AUTO_INCREMENT,
     title VARCHAR(300) UNIQUE NOT NULL,
-    salary DECIMAL unsigned NOT NULL,
-    department_id INT unsigned NOT NULL,
-    PRIMARY KEY (id)
-    CONSTRAINT fk_department 
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (role_id),
+     
     FOREIGN KEY (department_id) 
     REFERENCES department(id) 
     ON DELETE SET NULL 
@@ -20,18 +20,18 @@ CREATE TABLE role (
 CREATE TABLE employee (
 
     
-    id INT unsigned AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(300) NOT NULL,
     last_name VARCHAR(300) NOT NULL,
-    role_id INT unsigned,
-    manager_id INT unsigned,
-    PRIMARY KEY (id)
-    CONSTRAINT fk_role_id
+    role_id INT ,
+    manager_id INT ,
+    PRIMARY KEY (id),
+    
     FOREIGN KEY  (role_id)
     
     REFERENCES role(role_id) 
-    ON DELETE SET NULL
-    CONSTRAINT fk_manager_id
+    ON DELETE SET NULL,
+    
     FOREIGN KEY (manager_id)
      
     REFERENCES employee(id)
@@ -39,4 +39,10 @@ CREATE TABLE employee (
 );
 
 
-SELECT * FROM employeedb
+--SELECT * FROM employeedb
+
+
+
+
+
+
