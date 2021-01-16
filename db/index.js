0c2405ea869a710acf3441c3a92baf184a9e53bc
smedigo <smedigo@gmail.com>
2021-01-15 18:16:13 -0800

    worked on index.js front and back end

--- db/index.js
+++ db/index.js
@@ -11,63 +11,72 @@ class employeeDB {
     constructor(contructor) {
         this.connection = connection;
     }
+    findAllEmployees() {
+        // Find all employees, join with roles and departments to 
+        //display their roles, salaries, departments, managers
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id; "
+        );
+    }
+    // Find all employees except the given employee id 
+    findAllPossibleManagers(employeeId) {
+        return this.connection.query(
+            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+            employeeId
+        );
+
+    }
+    // Create a new employee 
+    createEmployee(employee) {
+        return this.connection.query("INSERT INTO employee SET ?", employee);
+    }
+
+
+    // Remove a new employee with given Id 
+    deleteEmployee(employeeId) {
+        return this.connection.query("DELETE FROM employee WHERE id = ?", employeeId)
+
+    }
+
+    // Update an employee with the given role 
+    updateEmployeeRole(employeeId, roleId) {
+        return this.connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [roleId, employeeId])
+    }
+
+    // Update the employee's given manager
+
+    updateEmployeeManager(employeeId, managerId) {
+        return this.connection.query("UPDATE employee SET manager_id = ? WHERE id = ?", [managerId, employeeId])
+    }
+
+    // find all roles, join with departments to display the department name
+    findAllRoles() {
+        return this.connection.query(
+            "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    // create new role
+
+    // remove a role from the database
+
+    // create new department
+
+    // remove a department
+
+    // Find all employees in a given department, join with roles to display titles
+
+    // Find all employees by manager, join with departments and roles to display titles and department names
+
 }
 
 // can we do without class and constructor
 
-findAllEmployees() {
-    // Find all employees, join with roles and departments to 
-    //display their roles, salaries, departments, managers
-    return this.connection.query (
-        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id; "
-    );
-}
-
-// Find all employees except the given employee id 
-
-
-// Create a new employee 
-createEmployee(employee) {
-    return this.connection.query("INSERT INTO employee SET ?", employee);
-}
-
-// Remove a new employee with given Id 
-deleteEmployee(employeeId){
-     return this.connection.query("DELETE FROM employee WHERE id = ?", employeeId)
-
-}
-
-
-// Update an employee with the given role 
-updateEmployeeRole(employeeId, roleId) {
-    return this.connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [roleId, employeeId])
-}
-
-// Update the employee's given manager
-
-updateEmployeeRole(employeeId,managerId) {
-    return this.connection.query("UPDATE employee SET manager_id = ? WHERE id = ?", [managerId, employeeId])
-}
+modeule.exports = new employeeDB(connection);
 
 
 
-getRole() {
-    return connection.query("SELECT * FROM role")
-
-   },
 
 
-
-getEmployee() {
-    return connection.query("SELECT * FROM employee")
-
-   },
-
-
-
-insertRole(data) {
-
-    return connection.query( "INSERT INTO role ?", data);
-},
 
 
--- index.js
+++ index.js
@@ -1,56 +1,66 @@
 const db = require("./db");
 const connection = require("./db/connection");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+require("console.table");
+// used for hiding information for security purposes
+require("dotenv").config()
 // const { createPromptModule } = require("inquirer");
 
-function askforAction() {
+connection.connect(function(err) {
+  if (err) throw err;
+init();
+});
+
+
+function init() {
     inquirer
       .prompt({
         name: "action",
         type: "list",
         message: "What would you like to do?",
         choices: [
-          "VIEW_DEPARTMENT",
-          "VIEW_ROLE",
-          "VIEW_EMPLOYEE",
-          "CREATE_ROLE",
-          "ADD_EMPLOYEE",
-          "ADD_ROLE",
-          "ADD_DEPARTMENT",
-          "UPDATE_ROLE",
+          "View All Employees",
+          "Find all employees except the given employee id",
+          "Create a new employee",
+          "Remove a new employee with given Id",
+          "Update an employee with the given role",
+          "Update the employee's given manager",
+          "find all roles, join with departments to display the department name",
+          
            "exit"
         ]
       })
 
       .then((res) => {
+
+        // call the appropriate function depending on what the user choses
         switch (res.action) {
-        case "VIEW_DEPARTMENT":
-          querybyDepartment();
+        case "View All Employees":
+          findAllEmployees();
           return;
   
-        case "VIEW_ROLE":
-          queryRole();
+        case "Find all employees except the given employee id":
+          findAllPossibleManagers();
           return;
   
-        case "VIEW_EMPLOYEE":
-          queryAll();
+        case "Create a new employee":
+        createEmployee();
           return;
 
-        case "CREATE_ROLE":
-        createRole();
+        case "Remove a new employee with given Id":
+          deleteEmployee();
         return;
 
-        case "ADD_EMPLOYEE":
-        addEmployee();
+        case "Update an employee with the given role":
+          updateEmployeeRole();
         return;
 
-        case "ADD_DEPARTMENT":
-        addDpartment();
+        case "Update the employee's given manager":
+          updateEmployeeManager();
         return;
 
-        case "UPDATE_ROLE":
-        changeRole();
+        case "find all roles, join with departments to display the department name":
+          findAllRoles();
         return;
 
   
@@ -61,57 +71,205 @@ function askforAction() {
       });
     }
 
-    function viewDepartment() {
-        db.getDepartment()
-        .then((results) =>{
+    function findAllEmployees() {
+      connection.query(
+        `SELECT employee.id, employee.first_name AS “first name”, employee.last_name AS “last name”,
+          role.title, role.salary, department.name AS “department”,
+          CONCAT(manager.first_name,” “,manager.last_name) AS “manager”
+          FROM employee
+          LEFT JOIN role ON employee.role_id = role.id
+          LEFT JOIN department ON role.department_id = department.id
+          LEFT JOIN employee manager ON manager.id = employee.manager_id;`,
+        function(err, res) {
+          console.table(res);
+          init();
+        }
+      );
+    }
+    function findAllPossibleManagers() {
 
-            console.table(results);
-        
-            askforAction();
-        
-        });
+      connection.query(
+        `SELECT CONCAT(manager.first_name,” “,manager.last_name) AS “manager”,
+        CONCAT(employee.first_name,” “,employee.last_name) AS “Employee”, role.title, role.salary,
+        department.name AS “department”
+          FROM employee
+          LEFT JOIN role ON employee.role_id = role.id
+          LEFT JOIN department ON role.department_id = department.id
+          LEFT JOIN employee manager ON manager.id = employee.manager_id
+          ORDER BY manager;`,
+        function(err, res) {
+          console.table(res);
+          init();
+        }
+      );
+
+
+
+
     }
 
-    function createRole() {
-        db.getDepartment()
-        .then((department) =>{
 
-        console.log(department);
+    function createEmployee() {
 
-        const departmentChoices = department.map( (department)=> ({
-                value: department.id,
-                label: department.name,
-            }) )
+connection.query("Select title, id FROM role", function(errRole, resRole) {
+  if (errRole) throw errRole;
+  connection.query(
+    `SELECT CONCAT(first_name, " ", last_name) AS ManagerName, id FROM employee;`,
+    function(errManager, resManager) {
+      if (errManager) throw errManager;
+      inquirer.prompt([{
+        name: "firstName",
+        type: "input",
+        message: "Enter the employee's first name."
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "Enter the employee's last name."
+      },
+      {
+        name: "employeeRole",
+        type: "list",
+        message: "Select employees role.",
+        choices: resRole.map(role => {
+          return {
+            name: role.title,
+            value: role.id
+          };
+        })
+      },
+      {
+        name: "employeeManager",
+        type: "list",
+        message: "Enter the employee's manager.",
+        choices: resManager.map(manager => {
+          return {
+            name: manager.MANAGERname,
+            value: manager.id
+          }
+        })
+      },
+    ]) .then(function(answer) {
+      connection.query (
+        "INSERT INTO employee SET ?",
+        {
+         first_name: answer.firstName,
+         last_name: answer.lastName,
+         role_id: answer.employeeRole,
+         manager_id: answer.employeeManager
+        },
+        function(err) {
+          if (err) throw err;
+          init();
+        }
+      );
+    });
+    }
+  )
+})
 
-        // console.log (
-        //      departments.map( (department)=> ({
-        //         value: department.id,
-        //         label: department.name,
-        //     }) )
-        // );
-
-
-         inquirer
-         .prompt([
-             {
-               message: " what department is this role for?", 
-               type: "list",
-               name: "department-id",
-               choices: departmentChoices
-               }
-            
-            
-         ]).then(res =>{
-
-            console.log(res);
-         });
-            
-        
-        });
 
     }
-    
-    askforAction();
+
+    function deleteEmployee () {
+
+    connection.query( `SELECT CONCAT(first_name," ", last_name) AS nameOfEmployee, id FROM employee;`,
+    function(err, res) {
+      if (err) throw err;
+      inquirer
+      .prompt([
+        {
+          name: "deleteEmployee",
+          type: "list",
+          message: "Select an employee to remove",
+          choices: res.map(employee => {
+            return {
+              name: employee.nameOfEmployee,
+              value: employee.id
+            };
+          })
+        }
+      ])
+      .then(function(answer) {
+        connection.query(
+          "DELETE FROM employee WHERE id = ?",
+          [answer.deleteEmployee],
+          function(err) {
+            if (err) throw err;
+            console.log("Employee removed!");
+            init();
+          }
+        );
+      });
+  }
+);
+}
+
+function updateEmployeeRole() {
+
+  connection.query(`SELECT CONCAT(first_name," ", last_name) AS nameOfEmployee, id FROM employee`,
+  function(errEmployee, resEmployee) {
+    if (errEmployee) throw errEmployee;
+    connection.query("Select title, id FROM role", function(
+      errRole,
+      resRole
+    ){
+      if (errRole) throw errRole;
+      inquirer.prompt([{
+        name: "nameOfEmployee",
+        type: "list",
+        message:"Select employee.",
+        choices: resEmployee.map(employee => {
+          return {
+            name: employee.nameOfEmployee,
+            value: employee.id
+          };
+        })
+      },
+
+      {
+        name: "employeeRole",
+        type: "lsit",
+        message:"Select employee's new role",
+        choices: resRole.map(role => {
+          return {
+            name: role.title,
+            value: role.id
+          };
+        })
+      },
+    ]) .then (function(answer) {
+      connection.query(
+        "UPDATE employee SET role_id = ? WHERE id = ?",
+        [answer.employeeRole, answer.nameOfEmployee],
+        function(err) {
+          if (err) throw err;
+          console.log("Employee role updated!");
+          init();
+        }
+      );
+    });
+    })
+
+  }
+  );
+
+
+}
+
+function updateEmployeeManager() {
+
+
+
+}
+
+function findAllRoles ()
+{
+
+} 
+
+
+askforAction();
 
 
 
